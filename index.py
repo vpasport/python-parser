f836d05696ae9910ee5010c0e5f27aef162e81e8
vpasport <pasportnikov2000@gmail.com>
2021-09-17 23:33:13 +0300

    update article parsing

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ driver = webdriver.Chrome('./chromedriver.exe')
 def get_page(url):
     driver.get(url)
 
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'search-results')))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'search-results')))
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
@@ -25,30 +25,62 @@ def parse_page(soup):
     articles = []
 
     for el in list:
-        articl = {}
+        article = {}
 
-        articl['link'] = 'https://cyberleninka.ru/' + el.find('a', href=True)['href']
-        articl['name'] = el.find('h2', {'class': 'title'}).get_text()
-        articl['author'] = el.find('span').get_text()
-        articl['year'], articl['branch'] = el.find('span', {'class': 'span-block'}).get_text().split(' / ')
-        articl['description'] = el.find('div').get_text()
+        article['link'] = 'https://cyberleninka.ru/' + el.find('a', href=True)['href']
+        article['name'] = el.find('h2', {'class': 'title'}).get_text()
+        article['year'], article['journalName'] = el.find('span', {'class': 'span-block'}).get_text().split(' / ')
 
-        articles.append(articl)
+        print(f"Parsing: {article['name']}")
+
+        driver.get(article['link'])
+
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'infoblock')))
+        # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'full abstract')))
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        article['author'] = []
+        authors = soup.find('ul', {'class': 'author-list'}).findAll('li')
+        for author in authors:
+            article['author'].append(author.get_text())
+
+        article['description'] = ''
+        description = soup.find('div', {'class': 'full abstract'})
+        try:
+            article['description'] = description.get_text()
+        except:
+            pass
+
+        article['keywords'] = []
+        keywords = soup.find('i', {'itemprop': 'keywords'})
+        try:
+            keywords = keywords.findAll('span')
+            for word in keywords:
+                article['keywords'].append(word.get_text())
+
+            article['branch'] = soup.find('div', {'class': 'half-right'}).find('a').get_text()
+        except:
+            pass
+
+        articles.append(article)
 
     return articles
 
 
-articles = []
+try:
+    articles = []
 
-for i in range(1, 11):
-    page = get_page(
-        f'https://cyberleninka.ru/search?q=%D0%B2%D0%B5%D0%B1-%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D0%B8&page={i}'
-    )
+    for i in range(1, 11):
+        page = get_page(
+            f'https://cyberleninka.ru/search?q=%D0%B2%D0%B5%D0%B1-%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D0%B8&page={i}'
+        )
 
-    articles = articles + parse_page(page)
+        articles = articles + parse_page(page)
 
-print(json.dumps(articles, indent=4, ensure_ascii=False))
-with open('data.json', 'w', encoding='utf-8') as f:
-    f.write(json.dumps(articles, indent=4, ensure_ascii=False))
-
-driver.close()
+    with open('data.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(articles, indent=4, ensure_ascii=False))
+finally:
+    driver.close()
